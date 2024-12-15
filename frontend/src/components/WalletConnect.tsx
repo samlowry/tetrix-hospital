b2afcbf831797e72a36d0b77693c73188a08ee0d
samlowry <samlowry@e-baka.net>
2024-12-15 13:41:27 +0100

    fixes

--- frontend/src/components/WalletConnect.tsx
+++ frontend/src/components/WalletConnect.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { TonConnectButton, useTonConnect } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
 import { api } from '../api';
 
 export const WalletConnect: React.FC = () => {
-    const { connector } = useTonConnect();
+    const [{ connector }] = useTonConnectUI();
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
